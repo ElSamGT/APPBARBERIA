1f2e8a26bddeabd614a4bb6845e5549b41b8979d
ElSamGT <119107031+ElSamGT@users.noreply.github.com>
2022-11-27 16:27:49 -0600

    Update CitaController.php

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -7,7 +7,9 @@ use MVC\Router;
 class CitaController {
     public static function index( Router $router ) {
 
-        session_start();
+        if(!isset($_SESSION)) {
+            session_start();
+        };
 
         isAuth();
 
